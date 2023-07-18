0cfbcff6b0cb807479d907701164320aa272b420
Oscar Suarez <Oscar.suarezwgd@gmail.com>
2023-07-17 21:56:51 -0600

    Se corrigieron errores de responsividad, se agregaron 2 contenedores más.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,6 +8,7 @@ import img2 from '@/assets/img/photo-2.jpg'
 import img3 from '@/assets/img/photo-3.jpg'
 import img4 from '@/assets/img/photo-4.jpg'
 import img5 from '@/assets/img/photo-5.jpg'
+import prod from '@/assets/img/production.avif'
 
 function About() {
 
@@ -19,7 +20,7 @@ function About() {
       <div className={styles.contAbout}>
 
         <div className={styles.contUno}>
-          <Image className={styles.imgH} src={imgH} ></Image>
+          <Image className={styles.imgH} src={imgH} />
           <p className={styles.titleAbl}>Ableton</p>
         </div>
         
@@ -34,10 +35,10 @@ function About() {
 
         <div className={styles.contTres}>
           <div className={styles.contYellow}>
-            <Image className={styles.img2} src={img2}></Image>
+            <Image className={styles.img2} src={img2}/>
           </div>
           <div className={styles.contWhite}>
-          <Image className={styles.img1} src={img1}></Image>
+          <Image className={styles.img1} src={img1}/>
           </div>
         </div>
 
@@ -66,22 +67,42 @@ function About() {
 
         <div className={styles.contSte}>
           <div className={styles.contGreen}>
-            <Image className={styles.img3} src={img3}></Image>
-            <Image className={styles.img4} src={img4}></Image>
+            <Image className={styles.img3} src={img3}/>
+            <Image className={styles.img4} src={img4}/>
           </div>
           <div className={styles.contWhite2}>
-          <Image className={styles.img5} src={img5}></Image>
+          <Image className={styles.img5} src={img5}/>
           </div>
         </div>
 
         <div className={styles.contOcho}>
           <h1>
-          We are more than 350 people from 30 different countries divided between our headquarters in Berlin and our offices in Los Angeles and Tokyo.
+          We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.
           </h1>
           <p>
-          Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture.
+          Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We’ve learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don’t always agree with each other, but opinion and debate are valued and openly encouraged.
           </p>
         </div>
+
+      <div className={styles.contNve}>
+        <Image className={styles.prod} src={prod}/>
+      </div>
+
+      <div className={styles.contDiez}>
+          <h1>
+          We’re passionate about what we do, but we’re equally passionate about improving who we are.
+          </h1>
+          <p>
+          We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.
+          </p>
+          <p>
+          Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.
+          </p>
+      </div>
+
+
+
+
       </div>
     </div>
   )
